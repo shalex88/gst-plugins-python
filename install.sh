7e46b7a1a1a58781333ebfd6fe4cdccd79307817
Alex Sh <shalex.work@gmail.com>
2024-07-10 15:07:01 +0300

    Update postprocessing

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 install_dependencies() {
     $SCRIPT_DIR/preprocessing/install.sh
-    # $SCRIPT_DIR/postprocessing/install.sh
+    $SCRIPT_DIR/postprocessing/install.sh
 }
 
 install_dependencies
